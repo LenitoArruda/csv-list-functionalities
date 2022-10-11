04554304b4e5622cbfd6f149d1a0f16bd316724d
LenitoArruda <lenitoarruda@hotmail.com>
2022-10-10 22:10:28 -0300

    Creating HTML and PDF Writer

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const Reader = require("./Reader");
+const Writer = require("./Writer");
+const Procesor = require("./Processor");
+const Table = require("./Table");
+const HtmlParser = require("./HtmlParser");
+const PdfWriter = require("./PdfWriter");
+
 const reader = new Reader();
+const writer = new Writer();
+
 
 
 async function main(){
     var data = await reader.Read("./users.csv");
-    console.log(data);
+    var processedData = Procesor.Process(data);
+    var users = new Table(processedData);
 
+    var html = await HtmlParser.Parser(users);
+    
+    writer.Write("index.html",html);
+    PdfWriter.WritePdf("index.pdf", html);
 }
 
 main();
